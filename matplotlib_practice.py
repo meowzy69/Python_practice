24d3f525299471caeb6db11b298a97b20bc03590
Chandrapal <chandumain01@gmail.com>
2025-05-27 20:40:02 +0530

    Updated matplotlib_practice.py with latest changes

--- matplotlib_practice.py
+++ matplotlib_practice.py
@@ -9,5 +9,57 @@ colors = ['yellow', 'blue', 'green', 'red', 'purple']
 plt.pie(Data, labels=Task, colors=colors, autopct='%1.1f%%')
 plt.title('Daily Activities')
 plt.legend(title='Activities')
-plt.show()
+plt.clf()  # Clear the current figure
 
+
+#Subplots and multiple graphs
+
+# Side by side line and bar chart
+x = [1, 2, 3, 4, 5]
+y1 = [10, 20, 25, 30, 35] #line
+y2 = [5, 15, 20, 25, 30] #bar
+
+#line plot 1st subplot
+plt.subplot(1, 2, 1)  # 1 row, 2 columns, 1st subplot
+plt.plot(x, y1, color='blue')
+plt.title('Line Chart')
+
+#bar plot 2nd subplot
+plt.subplot(1, 2, 2)  # 1 row, 2 columns, 2nd subplot
+plt.bar(x, y2, color='orange')
+plt.title('Bar Chart')
+
+plt.tight_layout()  # Adjust layout to prevent overlap
+plt.show()  # Display the plots
+
+
+# 2 rows, 2 columns , 4 charts
+
+x = [1, 2, 3, 4]
+y = [10, 20, 25, 30]
+
+plt.figure(figsize=(10, 6)) 
+
+#line
+plt.subplot(2, 2, 1)
+plt.plot(x, y, color='blue')
+plt.title('Line Chart')
+
+#bar
+plt.subplot(2, 2, 2)
+plt.bar(x, y, color='orange')
+plt.title('Bar Chart')
+
+#scatter
+plt.subplot(2, 2, 3)
+plt.scatter(x, y, color='green')
+plt.title('Scatter Plot')
+
+#pie
+plt.subplot(2, 2, 4)
+labels = ['A', 'B', 'C', 'D']
+plt.pie(y, labels=labels, autopct='%1.1f%%')
+plt.title('Pie Chart')
+
+plt.tight_layout()  
+plt.show()  
